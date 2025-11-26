d15bf88c00e130d747fe081cc1bbde70524537a9
Harry Liu <yxliu@uwaterloo.ca>
2025-11-26 18:02:09 -0500

    add destructor for printer

--- printer.h
+++ printer.h
@@ -30,6 +30,7 @@ _Monitor Printer {
   public:
     enum Kind { Parent, Groupoff, WATCardOffice, NameServer, Truck, BottlingPlant, Student, Vending, Courier };
     Printer( unsigned int numStudents, unsigned int numVendingMachines, unsigned int numCouriers );
+    ~Printer();
     void print( Kind kind, char state );
     void print( Kind kind, char state, unsigned int value1 );
     void print( Kind kind, char state, unsigned int value1, unsigned int value2 );
