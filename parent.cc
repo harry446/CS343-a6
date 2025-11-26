9d3089b2cd216fc5fd7602115993a5ed78a85eeb
z66li <z66li@uwaterloo.ca>
2025-11-26 18:19:01 -0500

    Adding more file

---
+++ parent.cc
@@ -0,0 +1,38 @@
+#include "parent.h"
+#include "bank.h"
+#include "printer.h"
+
+Parent::Parent(Printer& prt, Bank& bank, unsigned int numStudents,
+               unsigned int parentalDelay) : prt(prt), bank(bank), numStudents(numStudents), parentalDelay(parentalDelay)
+{
+    totalAmount = 0;
+}
+
+
+void Parent::main() {
+    prt.print(Printer::Kind::Parent, 'S');
+
+    while (true) {
+        // Parent must check for a call to its dtor
+        _Accept (~Parent) {
+            break; 
+        }
+        _ELSE {         // if no dtor being called
+            yield(parentalDelay);       // yield before transfer 
+            unsigned int selectedStudentID = prng(0, numStudents-1); 
+            unsigned int amount; 
+            if (prng(19) == 0) {    // 1 in 20 chance 
+                amount = 10; 
+                prt.print(Printer::Kind::Parent, 'E', selectedStudentID, amount);
+            } 
+            else {
+                amount = prng(1, 3); 
+                prt.print(Printer::Kind::Parent, 'D', selectedStudentID, amount);
+            }
+            bank.deposit(selectedStudentID, amount);       // bank deposit 
+            totalAmount += amount; 
+        }
+    }
+    prt.print(Printer::Kind::Parent, 'F', totalAmount);
+}
+
---
+++ parent.h
@@ -0,0 +1,20 @@
+#ifndef PARENT_H_
+#define PARENT_H_
+
+_Monitor Printer;
+_Monitor Bank;
+
+
+_Task Parent {
+    Printer & prt; 
+    Bank & bank; 
+    unsigned int numStudents; 
+    unsigned int parentalDelay; 
+    unsigned int totalGift; 
+	void main();
+  public:
+	Parent( Printer & prt, Bank & bank, unsigned int numStudents, unsigned int parentalDelay );
+};
+
+
+#endif
