9b1ff20887480d853b9d51a7660a77b5829b26e6
Harry Liu <yxliu@uwaterloo.ca>
2025-11-27 15:19:11 -0500

    nameServer implementation

---
+++ nameServer.cc
@@ -0,0 +1,52 @@
+#include "nameServer.h"
+#include "printer.h"
+#include "vendingMachine.h"
+
+NameServer::NameServer( Printer & prt, unsigned int numVendingMachines, unsigned int numStudents ): 
+    prt(prt), numVendingMachines(numVendingMachines), numStudents(numStudents) {
+    
+    machines = new VendingMachine*[numVendingMachines];
+    assignments = new unsigned int[numStudents];
+
+    // student 0 to machine 0, ..., student numVendingMachines to machine 0, student numVendingMachines+1 to machine 1, ...
+    for (int i=0; i<numStudents; i++) {
+        assignments[i] = i%numVendingMachines;   
+    } 
+}
+
+NameServer::~NameServer() {
+    delete [] machines;
+    delete [] assignments;
+}
+
+
+void NameServer::VMregister( VendingMachine * vendingmachine ) {
+    prt.print(Printer::Kind::NameServer, 'R', curMachineCnt);   // R v
+    machines[curMachineCnt++] = vendingmachine;
+}
+
+VendingMachine * NameServer::getMachine( unsigned int id ) __attribute__(( warn_unused_result )) {
+    unsigned int idx = assignments[id];
+    assignments[id] = (idx + 1) % numVendingMachines;     // assign student to the next machine
+    prt.print(Printer::Kind::NameServer, 'N', id, idx);   // N s,v
+    
+    return machines[idx];
+}
+
+VendingMachine ** NameServer::getMachineList() __attribute__(( warn_unused_result )) {
+    return machines;
+}
+
+
+void NameServer::main() {
+    prt.print(Printer::Kind::NameServer, 'S');
+    for ( ;; ) {
+        _Accept(~NameServer) {
+            break;
+        } or _When(curMachineCnt < numVendingMachines) _Accept(VMregister) {
+        } or _When(curMachineCnt == numVendingMachines) _Accept(getMachine || getMachineList) {
+        }
+    }
+
+    prt.print(Printer::Kind::NameServer, 'F');
+}
---
+++ nameServer.h
@@ -0,0 +1,26 @@
+#ifndef NAME_SERVER_H_
+#define NAME_SERVER_H_
+
+_Monitor Printer;
+_Task VendingMachine;
+
+_Task NameServer {
+    Prrinter & prt;
+    const unsigned int numVendingMachines;
+    const unsigned int numStudents;
+
+    VendingMachine ** machines;     // array of machine ptrs
+    unsigned int curMachineCnt = 0;
+
+    unsigned int * assignments;
+
+	void main();
+  public:
+	NameServer( Printer & prt, unsigned int numVendingMachines, unsigned int numStudents );
+    ~NameServer();
+	void VMregister( VendingMachine * vendingmachine );
+	VendingMachine * getMachine( unsigned int id ) __attribute__(( warn_unused_result ));
+	VendingMachine ** getMachineList() __attribute__(( warn_unused_result ));
+};
+
+#endif
